90af45909be9e3e7f756283c19020cc48fea3dbd
Tofumafu <chescahervera0317@gmail.com>
2024-03-11 13:43:37 +1030

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Choose an Operator: ");
+        Console.WriteLine();
+        string[] Operators = { "\t a = Addition", "\t s = Subtraction", "\t m = Multiplication", "\t d = Division" };
+        for (int i = 0; i < Operators.Length; i++)
+        {
+            Console.WriteLine(Operators[i]);
+        }
+
+        switch (Console.ReadLine())
+        {
+            case "a":
+            {
+                    Console.WriteLine("Type First Number: ");
+                    int number1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Type Second Number: ");
+                    int number2 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(number1 + number2);
+                    break;
+            }
+            case "s":
+            {
+                    Console.WriteLine("Type First Number: ");
+                    int number1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Type Second Number: ");
+                    int number2 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(number1 - number2);
+                    break;
+            }
+            case "m":
+            {
+                    Console.WriteLine("Type First Number: ");
+                    int number1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Type Second Number: ");
+                    int number2 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(number1 * number2);
+                    break;
+            }
+            case "d":
+            {
+                    Console.WriteLine("Type First Number: ");
+                    int number1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Type Second Number: ");
+                    int number2 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Answer: " + number1 / number2);
+                    break;
+            }
+        }
+    }   
+}
